72426992e994c48f1ac43862e9e66e86d33ea68f
panujaras14 <panujaras14@gmail.com>
2020-12-02 22:35:33 +0700

    api user

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,34 @@
 package com.panujaras
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
-}
 
+fun Application.module() {
+    install(DefaultHeaders)
+    install(CallLogging)
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    install(Routing) {
+        get(path = "/api/sea/user"){
+            val user= User(name="sea",age=50)
+            call.respond(user)
+
+        }
+    }
+}
+data class User(
+    var name:String? = null,
+    var age:Int? = null,
+
+)
